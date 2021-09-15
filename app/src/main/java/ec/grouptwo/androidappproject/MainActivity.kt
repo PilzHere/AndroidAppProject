0d785cf385fa038cbcf43b789cb59ac39689bcb2
carrotunderscore <robertpergustafsson@gmail.com>
2021-09-15 16:28:03 +0200

    Created code for database but not actual creatin. I'm stuck on how to actually intialize it.

--- app/src/main/java/ec/grouptwo/androidappproject/MainActivity.kt
+++ app/src/main/java/ec/grouptwo/androidappproject/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val database = DatabaseHandler(this)
-        database.onCreate(SQLiteDatabase.create())
+        //database.onCreate(SQLiteDatabase.create()) Vad gör man här??
 
     }
 
