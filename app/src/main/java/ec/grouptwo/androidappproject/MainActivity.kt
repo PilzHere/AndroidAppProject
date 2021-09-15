a1dbd381b6cd763b175e7d2fb97ca0b4c39e75f3
carrotunderscore <robertpergustafsson@gmail.com>
2021-09-15 15:15:11 +0200

    Created code for DB but not actual creation

--- app/src/main/java/ec/grouptwo/androidappproject/MainActivity.kt
+++ app/src/main/java/ec/grouptwo/androidappproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package ec.grouptwo.androidappproject
 
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val database = DatabaseHandler(this)
+        database.onCreate(SQLiteDatabase.create())
+
     }
+
 }
