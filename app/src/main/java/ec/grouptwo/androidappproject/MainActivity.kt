bd59cc5ecab9a37902a89914c2a08872e1bcc5e3
carrotunderscore <robertpergustafsson@gmail.com>
2021-09-16 15:57:21 +0200

    Cleaning up code...

--- app/src/main/java/ec/grouptwo/androidappproject/MainActivity.kt
+++ app/src/main/java/ec/grouptwo/androidappproject/MainActivity.kt
@@ -16,33 +16,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val database = DatabaseHandler(this)
-        val databaseHelper = database.writableDatabase // <------------------- This line causes the app to crash
-        /*
-        val values = ContentValues().apply{
-            put(Query.FeedEntry.USERS_USERID, "U100")
-            put(Query.FeedEntry.USERS_USERNAME, "jackCarrot")
-            put(Query.FeedEntry.USERS_PASSWORD, "password")
-            put(Query.FeedEntry.USERS_THEME, "Dark")
-        }
-        databaseHelper?.insert(Query.FeedEntry.TABLE_USERS, null, values)
-
-
-        checkDatabaseButton = findViewById<Button>(R.id.checkDB)
-        checkDatabaseButton.setOnClickListener{
-            val toast = Toast.makeText(applicationContext, print(isTableExists(databaseHelper, Query.FeedEntry.TABLE_USERS)).toString(), Toast.LENGTH_LONG)
-            toast.show()
-        }
-        isTableExists(databaseHelper, Query.FeedEntry.TABLE_USERS)
-
-         */
+        val databaseHelper = database.writableDatabase
 
     }
-    fun isTableExists(database: SQLiteDatabase, tableName: String): Boolean {
-        database.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = '$tableName'", null)?.use {
-            return it.count > 0
-        } ?: return false
-    }
-
 
 
 }
