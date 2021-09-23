0af129e648f41648288ac4594732af5e6bd73bd4
misterZink <heidarirobin@protonmail.com>
2021-09-23 11:41:07 +0200

    Added toolbar to searchgameactivity

--- app/src/main/java/ec/grouptwo/androidappproject/SearchGameActivity.kt
+++ app/src/main/java/ec/grouptwo/androidappproject/SearchGameActivity.kt
@@ -4,16 +4,12 @@ import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 
-    class SearchGameActivity : AppCompatActivity() {
+    class SearchGameActivity : BaseActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_search_game)
 
 
-        }
-        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-            menuInflater.inflate(R.menu.app_bar_menu, menu)
-            return true
         }
     }
 
